5ed0dd9cc20faee68f645737f0ae4c5939772eb7
laurenp-2 <laurenpothuru@gmail.com>
2024-11-17 13:00:54 -0500

    made a welcome popup

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,51 @@ import Upload from "./components/Upload";
 import Feed from "./components/Feed";
 import Profile from "./components/Profile";
 import Login from "./components/Login";
+import { ArrowRight } from "lucide-react";
+import {useState} from "react"; 
 
 function App() {
+
+  const [showWelcome, setShowWelcome] = useState(true); 
+
+    const handleExitClick = () => {
+        setShowWelcome(false); 
+    }
+
   return (
     <>
-    <div className="header">
-      <h1>SavorStats</h1>
-      <h3>Save your stats!</h3>
-    </div>
-      <NavBar />
-      <div>
-        <Routes>
-          <Route path="/" element={<Feed />} />
-          <Route path="/upload" element={<Upload />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
+
+    <div>
+      {showWelcome && (
+      <div className="welcomePopup"> 
+        <h1 id="welcomeHeader" className="cssanimation sequence fadeInBottom">Welcome to SavorStats!</h1>
+        <div className="welcomeExit">
+          <p>get started</p>
+          <ArrowRight onClick={handleExitClick}/>
+        </div>
       </div>
+       )}
+    </div>
+    <div>
+      {!showWelcome && (
+        <>
+        <div className="header">
+        <h1>SavorStats</h1>
+        <h3>Save your stats!</h3>
+      </div>
+        <NavBar />
+        <div>
+          <Routes>
+            <Route path="/" element={<Feed />} />
+            <Route path="/upload" element={<Upload />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+        </>
+      )}
+    </div>
+    
     </>
   );
 }
